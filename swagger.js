b219ac0b0df87cf587aeabb5118391c8f2d0022e
frannoi12 <francoistoyi4@gmail.com>
2024-11-25 00:27:45 +0000

    je pense que c'est ok

--- swagger.js
+++ swagger.js
@@ -55,5 +55,5 @@ const options = {
 const swaggerSpec = swaggerJsDoc(options);
 
 export function swaggerDocs(app) {
-    app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+    app.use("/", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 }
